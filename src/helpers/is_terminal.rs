a7534d5ccb0ad4751ad24ef102262ea83bdbe58d
BASED <basedpandit@pm.me>
2024-04-27 13:57:17 +0000

    Delete src directory

--- src/helpers/is_terminal.rs
+++
@@ -1,7 +0,0 @@
-pub fn is_terminal() -> bool {
-    std::process::Command::new("/bin/sh")
-        .args(["-c", "[ -t 0 ]"])
-        .status()
-        .unwrap()
-        .success()
-}
--- src/helpers/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod is_terminal;
-pub mod play_manager;
-pub mod provider_num;
-pub mod reqwests;
-pub mod selection;
